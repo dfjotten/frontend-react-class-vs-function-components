22134bcc3af8aceee0fe0528e5f210fb2740a7fa
Dan <dfjotten@gmail.com>
2021-05-24 17:06:01 +0200

    uitwerking

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Schrijf hier jouw functie-component!
-    </div>
-  );
+    const [isLightOn, toggleIsLightOn] = React.useState(true);
+
+    function handleClick() {
+        toggleIsLightOn(!isLightOn);
+    }
+
+    return (
+        <button
+            type="button"
+            onClick={handleClick}
+        >
+            {isLightOn ? 'AAN' : 'UIT'}
+        </button>
+    );
 }
 
 export default App;
